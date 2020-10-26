8aedc417793b54823c313295a1fcbdeda9602417
Tanvi <t51@my.centennialcollege.ca>
2020-10-26 19:54:09 -0400

    First Commit
    1. Changed mainActivity name
    2. Changed gitignore

---
+++ app/src/main/java/tanvi/tanvi/s301044096/tanviActivity.java
@@ -0,0 +1,14 @@
+package tanvi.tanvi.s301044096;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tanviActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
