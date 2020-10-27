eb18ee2100386014566986ae7b8cb1147cb91a10
Tanvi <t51@my.centennialcollege.ca>
2020-10-26 23:38:21 -0400

    Third Commit
    Created three fragments and the tabs are functional. Content in the tabs is still to be done.

--- app/src/main/java/tanvi/tanvi/s301044096/tanviActivity.java
+++ app/src/main/java/tanvi/tanvi/s301044096/tanviActivity.java
@@ -1,8 +1,13 @@
 package tanvi.tanvi.s301044096;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class tanviActivity extends AppCompatActivity {
 
@@ -10,5 +15,33 @@ public class tanviActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.tanvi_bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.tanvi_draw:
+                            selectedFragment = new tanvifragment();
+                            break;
+                        case R.id.tanvi_animate:
+                            selectedFragment = new tanviifragment();
+                            break;
+                        case R.id.tanvi_transform:
+                            selectedFragment = new s301044096fragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.tanvi_fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
